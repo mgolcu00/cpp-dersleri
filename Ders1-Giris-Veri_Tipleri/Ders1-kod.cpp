2ee7cc80eb01545d7fda2529fdc70bc72a9f88fc
mgolcu <mgolcu00@gmail.com>
2020-05-13 16:18:18 +0300

    Ders 1 Eklendi

---
+++ Ders1-Giris-Veri_Tipleri/Ders1-kod.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    //mertin verileri
+    int mert_yas=20; //0x101 -> 20
+    int mert_kilo =80;
+    float mert_boy = 1.90f;
+    //ahmetin verileri 
+    int ahmet_yas=21;
+    int ahmet_kilo =82;
+    float ahmet_boy=1.90f;
+
+    mert_yas=21;//0x101 -> 21
+    
+
+
+    return 0;
+}
