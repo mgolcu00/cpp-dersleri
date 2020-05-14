19e112e26a6873824e803274f32a48e0f5b40cbe
mgolcu <mgolcu00@gmail.com>
2020-05-14 22:00:00 +0300

    Ders 2 add

---
+++ Ders2-ilkprogram/ilkprogram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int sayi;
+    float kayannoktlisayi;
+    double buyukkayannoktalisayi;
+    char karakter;
+    bool mantik;
+
+    sayi =20;
+    cout<<sayi<<endl;
+
+    float pi = 3.145f;
+    cout<<"pi = "<<pi<<endl;
+
+    karakter = 'A';
+    cout<<"karakterim = "<<karakter<<endl;
+
+    //c -> screen , out -> çıkarmak ,dışarı 
+    return 0;
+}
